ea100747ebac8435858f0d33ce87b8c9cdb09193
nsteinme <nick.steinmetz@gmail.com>
2018-06-04 14:24:45 +0100

    add required functions to mexGPUall

--- CUDA/mexGPUall.m
+++ CUDA/mexGPUall.m
@@ -6,6 +6,9 @@
     mexcuda -largeArrayDims mexSVDsmall.cu
     mexcuda -largeArrayDims mexWtW2.cu
     mexcuda -largeArrayDims mexFilterPCs.cu
+    mexcuda -largeArrayDims mexClustering.cu
+    mexcuda -largeArrayDims mexDistances.cu
+
 
 %    mex -largeArrayDims mexMPmuFEAT.cu
 %    mex -largeArrayDims mexMPregMU.cu
