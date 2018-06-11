0f43c9a8bf7f4873dca60abe73746becb9a56ab7
Marius Pachitariu <marius10p@gmail.com>
2018-06-11 12:55:15 -0400

    wrong file added to mexGPUall

--- CUDA/mexGPUall.m
+++ CUDA/mexGPUall.m
@@ -2,7 +2,7 @@
 % Matlab GPU library first (see README files for platform-specific
 % information)
     mexcuda -largeArrayDims mexGetSpikes.cu
-    mexcuda -largeArrayDims mexMPnu8b.cu
+    mexcuda -largeArrayDims mexMPnu8.cu
     mexcuda -largeArrayDims mexSVDsmall2.cu
     mexcuda -largeArrayDims mexWtW2.cu
     mexcuda -largeArrayDims mexFilterPCs.cu
