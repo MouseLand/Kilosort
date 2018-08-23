77bd4851430f4de9c7f6082063ec273cad637108
Marius Pachitariu <marius10p@gmail.com>
2018-08-23 17:50:55 -0400

    fixed bug

--- mainLoop/memorizeW.m
+++ mainLoop/memorizeW.m
@@ -8,5 +8,6 @@ rez.U = gather(U);
 rez.mu = gather(mu);
 
 for n = 1:size(U,2)
-    rez.Wraw(:,:,n) = mu(n) * sq(Urot(:,n,:)) * sq(rez.W(:,n,:))';
+    % temporarily use U rather Urot until I have a chance to test it
+    rez.Wraw(:,:,n) = mu(n) * sq(U(:,n,:)) * sq(rez.W(:,n,:))';
 end
