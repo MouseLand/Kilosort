85b2796c384321e2e1cd2bbc6ca91dd05c3dba10
Marius Pachitariu <marius10p@gmail.com>
2019-01-30 20:25:52 -0500

    removed some unnecessary files

--- mainLoop/learnAndSolve8b.m
+++ mainLoop/learnAndSolve8b.m
@@ -8,7 +8,7 @@ wPCA = gpuArray(wPCA);
 
 ops.wPCA = wPCA;
 rez.ops = ops;
-% wPCA = gpuArray(ops.wPCA(:,1:3)); 
+% wPCA = gpuArray(ops.wPCA(:,1:3));
 
 wPCA(:,1) = - wPCA(:,1) * sign(wPCA(20,1));
 
@@ -20,7 +20,7 @@ Nnearest    = 32;
 sigmaMask  = ops.sigmaMask;
 
 
-ops.spkTh = -6;  
+ops.spkTh = -6;
 
 nt0 = ops.nt0;
 nt0min  = ceil(20 * nt0/61);
@@ -29,7 +29,7 @@ rez.ops.nt0min  = nt0min;
 nBatches  = rez.temp.Nbatch;
 NT  	= ops.NT;
 batchstart = 0:NT:NT*nBatches;
-Nfilt 	= ops.Nfilt; 
+Nfilt 	= ops.Nfilt;
 
 Nrank   = 3; %ops.Nrank;
 Nchan 	= ops.Nchan;
@@ -43,17 +43,17 @@ nhalf = ceil(nBatches/2);
 ischedule = [nhalf:nBatches nBatches:-1:nhalf];
 i1 = [(nhalf-1):-1:1];
 i2 = [nhalf:nBatches];
-    
+
 irounds = cat(2, ischedule, i1, i2);
 
-niter   = numel(irounds); 
+niter   = numel(irounds);
 if irounds(niter - nBatches)~=nhalf
     error('mismatch between number of batches');
 end
 
 flag_resort      = 1;
 
-t0 = ceil(rez.ops.trange(1) * ops.fs);    
+t0 = ceil(rez.ops.trange(1) * ops.fs);
 
 nInnerIter  = 20;
 
@@ -88,26 +88,26 @@ m0 = ops.minFR * ops.NT/ops.fs;
 
 for ibatch = 1:niter
     %     k = irounds(ibatch);
-    korder = irounds(ibatch);    
-    k = isortbatches(korder); 
-    
+    korder = irounds(ibatch);
+    k = isortbatches(korder);
+
     if ibatch>niter-nBatches && korder==nhalf
         [W, dWU, sig] = revertW(rez);
         fprintf('reverted back to middle timepoint \n')
     end
-    
+
     if ibatch<=niter-nBatches
         Params(9) = pmi(ibatch);
         pm = pmi(ibatch) * gpuArray.ones(1, Nfilt, 'single');
     end
-    
+
     % dat load \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    
+
     offset = 2 * ops.Nchan*batchstart(k);
     fseek(fid, offset, 'bof');
     dat = fread(fid, [NT ops.Nchan], '*int16');
     dataRAW = single(gpuArray(dat))/ ops.scaleproc;
-    
+
     if ibatch==1
         dWU = mexGetSpikes(Params, dataRAW, wPCA);
         dWU = reshape(wPCA * (wPCA' * dWU(:,:)), size(dWU));
@@ -118,11 +118,11 @@ for ibatch = 1:niter
         dnext(1:Nfilt) = 5^2;
         Params(2) = Nfilt;
     end
-    
+
     if flag_resort
         [~, iW] = max(abs(dWU(nt0min, :, :)), [], 2);
         iW = int32(squeeze(iW));
-        
+
         [iW, isort] = sort(iW);
         W = W(:,isort, :);
         dWU = dWU(:,:,isort);
@@ -130,80 +130,80 @@ for ibatch = 1:niter
         sig = sig(isort);
         dnext = dnext(isort);
     end
-    
+
     % decompose dWU by svd of time and space (61 by 61)
     [W, U, mu] = mexSVDsmall2(Params, dWU, W, iC-1, iW-1, Ka, Kb);
-    
+
     % this needs to change
     [UtU, maskU] = getMeUtU(iW, iC, mask, Nnearest, Nchan);
 
     [st0, id0, x0, featW, dWU, drez, nsp0, ss0, featPC, sig, dnext] = ...
         mexMPnu8(Params, dataRAW, dWU, U, W, mu, iC-1, iW-1, UtU, iList-1, ...
-        wPCA, maskU, pm, sig, dnext);    
-    
+        wPCA, maskU, pm, sig, dnext);
+
     nsp = nsp * p1 + (1-p1) * nsp0;
-    
+
     % \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    if ibatch==niter-nBatches                
+    if ibatch==niter-nBatches
         flag_resort   = 0;
-        
+
         % final clean up
         [W, U, dWU, mu, nsp, sig, dnext] = ...
-            triageTemplates2(ops, iW, C2C, W, U, dWU, mu, nsp, sig, dnext);        
-        
+            triageTemplates2(ops, iW, C2C, W, U, dWU, mu, nsp, sig, dnext);
+
         % run the algorithm locally to get splits
 %         [W, dWU, isplit] = halfwaySplits(rez, Params, W, U, mu,...
-%             dWU, sig,  fid, korder);        
+%             dWU, sig,  fid, korder);
 %         nsp     = nsp(isplit);
 %         sig     = sig(isplit);
 %         dnext   = dnext(isplit);
 %         mu      = mu(isplit);
 
         Nfilt = size(W,2);
-        Params(2) = Nfilt; 
-        
+        Params(2) = Nfilt;
+
         [WtW, iList] = getMeWtW(W, U, Nnearest);
-        
+
         [~, iW] = max(abs(dWU(nt0min, :, :)), [], 2);
         iW = int32(squeeze(iW));
-        
+
         % extract ALL features on the last pass
         Params(13) = 2;
-        
+
         % different threshold on last pass?
         Params(3) = ops.Th(end);
-        
+
         rez = memorizeW(rez, W, dWU, U, mu, sig);
         fprintf('memorized middle timepoint \n')
     end
-    
+
     if ibatch<niter-nBatches %-50
-        if rem(ibatch, 5)==1    
+        if rem(ibatch, 5)==1
             % this drops templates
             [W, U, dWU, mu, nsp, sig, dnext] = ...
                 triageTemplates2(ops, iW, C2C, W, U, dWU, mu, nsp, sig, dnext);
         end
         Nfilt = size(W,2);
         Params(2) = Nfilt;
-        
+
         % this adds templates
         dWU0 = mexGetSpikes(Params, drez, wPCA);
-        
+
         if size(dWU0,3)>0
             dWU0 = reshape(wPCA * (wPCA' * dWU0(:,:)), size(dWU0));
             dWU = cat(3, dWU, dWU0);
-            
+
             W(:,Nfilt + [1:size(dWU0,3)],:) = W0(:,ones(1,size(dWU0,3)),:);
-            
+
             nsp(Nfilt + [1:size(dWU0,3)]) = ops.minFR * NT/ops.fs;
             mu(Nfilt + [1:size(dWU0,3)])  = 10;
             sig(Nfilt + [1:size(dWU0,3)])  = 5^2;
             dnext(Nfilt + [1:size(dWU0,3)])  = 5^2;
-            
+
             Nfilt = min(ops.Nfilt, size(W,2));
             Params(2) = Nfilt;
-            
+
             W   = W(:, 1:Nfilt, :);
             dWU = dWU(:, :, 1:Nfilt);
             nsp = nsp(1:Nfilt);
@@ -211,12 +211,12 @@ for ibatch = 1:niter
             sig  = sig(1:Nfilt);
             dnext  = dnext(1:Nfilt);
         end
-        
+
     end
-    
+
     if ibatch>niter-nBatches
         ioffset         = ops.ntbuff;
-        if k==1 
+        if k==1
             ioffset         = 0;
         end
         toff = nt0min + t0 -ioffset + (NT-ops.ntbuff)*(k-1);
@@ -226,41 +226,41 @@ for ibatch = 1:niter
         st3(irange,2) = double(id0+1);
         st3(irange,3) = double(x0);
         st3(irange,4) = double(ss0(:,1));
-        
-        fW(:, irange) = gather(featW);        
-        
+
+        fW(:, irange) = gather(featW);
+
         fWpc(:, :, irange) = gather(featPC);
-        
+
         ntot = ntot + numel(x0);
     end
-    
-    if ibatch==niter-nBatches        
-        
+
+    if ibatch==niter-nBatches
+
         st3 = zeros(1e7, 4);
         fW  = zeros(Nnearest, 1e7, 'single');
         fWpc = zeros(NchanNear, Nrank, 1e7, 'single');
     end
-    
+
     if rem(ibatch, 100)==1
         fprintf('%2.2f sec, %d / %d batches, %d units, nspks: %2.2f, mu: %2.2f, nst0: %d \n', ...
             toc, ibatch, niter, Nfilt, sum(nsp), median(mu), numel(st0))
-        
+
 %        figure(2)
 %        subplot(2,2,1)
 %        imagesc(W(:,:,1))
-%        
+%
 %        subplot(2,2,2)
 %        imagesc(U(:,:,1))
-%        
+%
 %        subplot(2,2,3)
 %        plot(mu)
 %        ylim([0 100])
-%        
+%
 %        subplot(2,2,4)
 %        semilogx(1+nsp, mu, '.')
 %        ylim([0 100])
 %        xlim([0 100])
-%        
+%
 %        drawnow
     end
 end
@@ -299,4 +299,3 @@ rez.cProjPC     = permute(fWpc, [3 2 1]); %zeros(size(st3,1), 3, nNeighPC, 'sing
 % [~, iNch]       = sort(abs(rez.U(:,:,1)), 1, 'descend');
 maskPC          = zeros(Nchan, Nfilt, 'single');
 rez.iNeighPC    = gather(iC(:, iW));
-
--- masterBatch.m
+++ masterBatch.m
@@ -1,16 +1,16 @@
-addpath(genpath('D:\GitHub\KiloSort2')) % path to kilosort folder
-addpath('D:\GitHub\npy-matlab')
+addpath(genpath('D:/GitHub/KiloSort2')) % path to kilosort folder
+addpath('D:/GitHub\npy-matlab')
 
-pathToYourConfigFile = 'D:\GitHub\KiloSort2\configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+pathToYourConfigFile = 'D:/GitHub/KiloSort2/configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
 run(fullfile(pathToYourConfigFile, 'configFile384.m'))
 
 chanMapList = {'neuropixPhase3A_kilosortChanMap', ...
     'neuropixPhase3B1_kilosortChanMap', 'neuropixPhase3B2_kilosortChanMap'};
 
 fdir = {'Loewi\posterior', 'Loewi\frontal', 'WillAllen', ...
-    'Waksman\ZO', 'Waksman\K1', 'Waksman\ZNP1', ...
-    'Robbins\K1', 'Robbins\K3', 'Robbins\ZNP1', ...
-    'Josh\probeA', 'Josh\probeF', 'Josh\probeD', 'Bekesy\ZO', 'Bekesy\K1'};
+    'Waksman/ZO', 'Waksman/K1', 'Waksman/ZNP1', ...
+    'Robbins/K1', 'Robbins/K3', 'Robbins/ZNP1', ...
+    'Josh\probeA', 'Josh\probeF', 'Josh\probeD', 'Bekesy/ZO', 'Bekesy/K1'};
 NTOT = [384 384 385 385 385 385 385 385 385 384 384 384 385 385];
 iMap = [1 1 1 1 1 1 1 1 1 1 1 1 3 2];
 %%
@@ -20,30 +20,30 @@ ops.sorting     = 2; % type of sorting, 2 is by rastermap, 1 is old
 ops.trange      = [0 1900]; % TIME RANGE IN SECONDS TO PROCESS
 
 % find the binary file in this folder
-rootrootZ = 'F:\Spikes\';
+rootrootZ = 'F:/Spikes\';
 
 % path to whitened, filtered proc file (on a fast SSD)
 rootH = 'H:\DATA\Spikes\temp\';
 ops.fproc       = fullfile(rootH, 'temp_wh.dat'); % proc file on a fast SSD
 
 
-for j = 14 %1:numel(fdir)    
+for j = 1 %1:numel(fdir)
     fprintf('%s\n', fdir{j})
-    
+
     ops.chanMap = fullfile(pathToYourConfigFile, [chanMapList{iMap(j)} '.mat']);
     ops.NchanTOT    = NTOT(j); % total number of channels in your recording
     rootZ = fullfile(rootrootZ, fdir{j});
-    
+
     fs          = [dir(fullfile(rootZ, '*.bin')) dir(fullfile(rootZ, '*.dat'))];
     fname       = fs(1).name;
     ops.fbinary = fullfile(rootZ,  fname);
     ops.rootZ   = rootZ;
-    
+
     % preprocess data to create temp_wh.dat
     rez = preprocessDataSub(ops);
-    
+
     % pre-clustering to re-order batches by depth
-    fname = fullfile(rootZ, 'rez.mat');    
+    fname = fullfile(rootZ, 'rez.mat');
     if exist(fname, 'file')
         % just load the file if we already did this
         dr = load(fname);
@@ -59,20 +59,18 @@ for j = 14 %1:numel(fdir)
     rez = learnAndSolve8b(rez);
 
     % final splits
-    rez = splitAllClusters(rez);    
-    
+    rez = splitAllClusters(rez);
+
     % this saves to Phy
     rezToPhy(rez, rootZ);
-    
+
     % discard features in final rez file (too slow to save)
     rez.cProj = [];
     rez.cProjPC = [];
-    
-    % save final results as rez2 
+
+    % save final results as rez2
     fname = fullfile(rootZ, 'rez2.mat');
     save(fname, 'rez', '-v7.3');
-    
+
 %     loadManualSorting;
 end
-
-
