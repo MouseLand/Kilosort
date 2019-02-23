ab458de1f4fc526d1207b061eec44e6b6558872e
Nick Steinmetz <nick.steinmetz@gmail.com>
2019-02-23 15:20:31 -0800

    fix the way to skip get_good_channels

--- preProcess/preprocessDataSub.m
+++ preProcess/preprocessDataSub.m
@@ -15,22 +15,26 @@ ops.twind = ops.tstart * NchanTOT*2;
 Nbatch      = ceil(ops.sampsToRead /(NT-ops.ntbuff));
 ops.Nbatch = Nbatch;
 
-if getOr(ops, 'throw_out_channels', 1)
-    [chanMap, xc, yc, kcoords, NchanTOTdefault] = loadChanMap(ops.chanMap);
-end
+[chanMap, xc, yc, kcoords, NchanTOTdefault] = loadChanMap(ops.chanMap);
 ops.NchanTOT = getOr(ops, 'NchanTOT', NchanTOTdefault);
 
-% determine bad channels
-fprintf('Time %3.0fs. Determining good channels.. \n', toc);
 
-igood = get_good_channels(ops, chanMap);
-xc = xc(igood);
-yc = yc(igood);
-kcoords = kcoords(igood);
-chanMap = chanMap(igood);
+if getOr(ops, 'throw_out_channels', 1)    
+    
+    % determine bad channels
+    fprintf('Time %3.0fs. Determining good channels.. \n', toc);
 
+    igood = get_good_channels(ops, chanMap);
+    xc = xc(igood);
+    yc = yc(igood);
+    kcoords = kcoords(igood);
+    chanMap = chanMap(igood);
+        
+    ops.igood = igood;
+else
+    ops.igood = true(size(chanMap));
+end
 ops.Nchan = numel(chanMap);
-ops.igood = igood;
 
 rez.ops         = ops;
 rez.xc = xc;
