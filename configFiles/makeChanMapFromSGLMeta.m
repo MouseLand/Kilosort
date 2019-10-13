2ea2ca755a4051b056d515946d947f8a83603d72
Nick Steinmetz <nick.steinmetz@gmail.com>
2019-10-13 14:09:53 -0700

    add util for making a channel map for np2.0 probes, and one such map
    Former-commit-id: 86c9bdd858f62ffac700cabce19db44f1f00dffa

---
+++ configFiles/makeChanMapFromSGLMeta.m
@@ -0,0 +1,34 @@
+
+
+function cm = makeChanMapFromSGLMeta(m)
+
+shankMap = m.snsShankMap; 
+
+nCh = str2num(m.acqApLfSy);
+
+chanMap = [1:nCh(1)]'; 
+chanMap0ind = chanMap-1;
+connected = true(size(chanMap)); 
+
+shankSep = 250; 
+rowSep = 15; 
+colSep = 32;
+
+openParen = find(shankMap=='('); 
+closeParen = find(shankMap==')'); 
+for c = 1:nCh(1)
+    thisMap = shankMap(openParen(c+1)+1: closeParen(c+1)-1); 
+    thisMap(thisMap==':') = ',';
+    n = str2num(thisMap); 
+    xcoords(c) = (n(1)-1)*shankSep + (n(2)-1)*colSep; 
+    ycoords(c) = (n(3)-1)*rowSep; 
+end
+
+cm = struct();
+cm.chanMap = chanMap; 
+cm.chanMap0ind = chanMap0ind;
+cm.xcoords = xcoords'; 
+cm.ycoords = ycoords'; 
+cm.connected = connected;
+[~,name] = fileparts(m.imRoFile); 
+cm.name = name;
