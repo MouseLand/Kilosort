8e455cab002e0482f4c9dc7544d9b75e760cfef5
jenniferColonell <colonellj@janelia.hhmi.org>
2020-04-12 00:43:48 -0400

    Change threading in average_snips to thread over filters but not features. Fixes collision issue in summing elements of d_dWU.
    Former-commit-id: a9d8b6feef6d4e38aaf8703fd36601db7d4b2ada

--- CUDA/mexClustering2.cu
+++ CUDA/mexClustering2.cu
@@ -115,6 +115,39 @@ __global__ void average_snips(const double *Params, const int *iC, const int *ca
       }  
    }
 
+//////////////////////////////////////////////////////////////////////////////////////////
+__global__ void average_snips_v2(const double *Params, const int *iC, const int *call, 
+        const int *id, const float *uproj, const float *cmax, float *WU){
+  
+            
+  // jic, version with no threading over features, to avoid 
+  // collisions when summing WU 
+
+  int my_chan, this_chan, bid, ind, Nspikes, NrankPC, NchanNear, Nchan;
+  float xsum = 0.0f; 
+  int chanIndex, pcIndex;
+  
+  Nspikes               = (int) Params[0];
+  NrankPC             = (int) Params[1];  
+  Nchan                = (int) Params[7];
+  NchanNear             = (int) Params[6];
+    
+
+  bid 		= blockIdx.x;
+  
+  for(ind=0; ind<Nspikes;ind++)
+      if (id[ind]==bid){     
+          my_chan = call[ind];  
+          for (chanIndex = 0; chanIndex < NchanNear; ++chanIndex) {
+            this_chan = iC[chanIndex + NchanNear * my_chan];
+            for (pcIndex = 0; pcIndex < NrankPC; ++pcIndex) {
+                xsum = uproj[pcIndex + NrankPC*chanIndex +  NrankPC*NchanNear * ind];
+                WU[pcIndex + NrankPC*this_chan + NrankPC*Nchan * bid] +=  xsum;
+            }
+          }
+                    
+      }  
+   }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -319,10 +352,12 @@ void mexFunction(int nlhs, mxArray *plhs[],
   bestFilter<<<40, 256>>>(d_Params, d_iMatch, d_iC, d_call, d_cmax, d_id, d_x);
   
   // average all spikes for same template -- ORIGINAL
-  dim3 thNN(NrankPC, NchanNear);
-  average_snips<<<Nfilters, thNN>>>(d_Params, d_iC, d_call, d_id, d_uproj, d_cmax, d_dWU);
+  //dim3 thNN(NrankPC, NchanNear);
+  //average_snips<<<Nfilters, thNN>>>(d_Params, d_iC, d_call, d_id, d_uproj, d_cmax, d_dWU);
 
-
+  // average all spikes for same template -- threaded over filters, but not features
+  // avoid collision when adding to elements of d_dWU
+  average_snips_v2<<<Nfilters, 1>>>(d_Params, d_iC, d_call, d_id, d_uproj, d_cmax, d_dWU);
 
   //-------------------------------------------------
   //jic for running average_snips_v3 with Nfeature threads  
