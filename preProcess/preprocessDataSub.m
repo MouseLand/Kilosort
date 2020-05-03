3fac1131dfb121bb11edea692caeaee9875a3fcf
jenniferColonell <colonellj@janelia.hhmi.org>
2020-05-03 19:34:58 -0400

    remove comments
    Former-commit-id: 6c5639a36e71466d398032f25a9dd4784ed039c9

--- preProcess/preprocessDataSub.m
+++ preProcess/preprocessDataSub.m
@@ -8,7 +8,6 @@ function rez = preprocessDataSub(ops)
 % 4) channel whitening;
 % 5) scaling to int16 values
 
-fprintf( 'running KS2 in determ branch\n' );
 tic;
 ops.nt0 	  = getOr(ops, {'nt0'}, 61); % number of time samples for the templates (has to be <=81 due to GPU shared memory)
 ops.nt0min  = getOr(ops, 'nt0min', ceil(20 * ops.nt0/61)); % time sample where the negative peak should be aligned
