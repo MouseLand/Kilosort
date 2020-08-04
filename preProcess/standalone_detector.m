a102d5ffceec223cd4316eef7acddaaf5f2c4e20
Marius Pachitariu <marius10p@gmail.com>
2020-08-03 22:20:49 -0400

    added some comments
    Former-commit-id: cec674c6c9f6ece994ffc147f10c69de49f9c0fe

--- preProcess/standalone_detector.m
+++ preProcess/standalone_detector.m
@@ -1,54 +1,75 @@
 function [st3] = standalone_detector(rez, spkTh)
+% Detects spikes across the entire recording using generic templates.
+% Each generic template has rank one (in space-time).
+% In time, we use the 1D template prototypes found in wTEMP. 
+% In space, we use Gaussian weights of several sizes, centered 
+% on (x,y) positions that are part of a super-resolution grid covering the
+% entire probe (pre-specified in the calling function). 
+% In total, there ~100x more generic templates than channels. 
 
-sig = 10;
-dNearActiveSite = 30;
+
 ops = rez.ops;
 
+% minimum/base sigma for the Gaussian. 
+sig = 10;
+
+% grid of centers for the generic tempates
 [ycup, xcup] = meshgrid(ops.yup, ops.xup);
 
+% determine prototypical timecourses by clustering of simple threshold crossings. 
 NrankPC = 6;
 [wTEMP, wPCA]    = extractTemplatesfromSnippets(rez, NrankPC);
 
+% Get nearest channels for every template center. 
+% Template products will only be computed on these channels. 
 NchanNear = 10;
 [iC, dist] = getClosestChannels2(ycup, xcup, rez.yc, rez.xc, NchanNear);
 
+% Templates with centers that are far from an active site are discarded
+dNearActiveSite = 30; 
 igood = dist(1,:)<dNearActiveSite;
 iC = iC(:, igood);
 dist = dist(:, igood);
-
 ycup = ycup(igood);
 xcup = xcup(igood);
 
+% number of nearby templates to compare for local template maximum
 NchanNearUp =  10*NchanNear;
 [iC2, dist2] = getClosestChannels2(ycup, xcup, ycup, xcup, NchanNearUp);
 
+% pregenerate the Gaussian weights used for spatial components 
 nsizes = 5;
 v2 = gpuArray.zeros(5, size(dist,2), 'single');
 for k = 1:nsizes
     v2(k, :) = sum(exp( - 2 * dist.^2 / (sig * k)^2), 1);
 end
 
+% build up Params
 NchanUp = size(iC,2);
-
 Params = [ops.NT ops.Nchan ops.nt0 NchanNear NrankPC ops.nt0min spkTh NchanUp NchanNearUp sig];
 
+% preallocate the results
 st3 = zeros(1000000, 5);
-t0 = ceil(rez.ops.trange(1) * ops.fs);
-nsp = 0;
+t0 = 0; %ceil(rez.ops.trange(1) * ops.fs); % I think this should be 0 all the time. 
+nsp = 0; % counter for total number of spikes
 
 tic
 for k = 1:ops.Nbatch
+    % get a batch of whitened and filtered data
     dataRAW = get_batch(rez.ops, k);
     
+    % run the CUDA function on this batch
     [dat, kkmax, st, cF] = spikedetector3(Params, dataRAW, wTEMP, iC-1, dist, v2, iC2-1, dist2);
     
-    % compute y position    
+    % upsample the y position using the center of mass of template products
+    % coming out of the CUDA function. 
     ys = rez.yc(iC);
     cF0 = max(0, cF);
     cF0 = cF0 ./ sum(cF0, 1);
     iChan = st(2, :) + 1;
     yct = sum(cF0 .* ys(:, iChan), 1);
     
+    % build st for the current batch
     st = double(gather(st));
     st(2,:) = gather(yct);
     
@@ -57,13 +78,13 @@ for k = 1:ops.Nbatch
         ioffset         = 0; % the first batch is special (no pre-buffer)
     end
     toff = ops.nt0min + t0 -ioffset + (ops.NT-ops.ntbuff)*(k-1);
-    st(1,:) = st(1,:) + toff;
+    st(1,:) = st(1,:) + toff; % these offsets ensure the times are computed correctly
     
-    st(5,:) = k;
+    st(5,:) = k; % batch number
     
     nsp0 = size(st,2);
     if nsp0 + nsp > size(st3,1)
-       st3(nsp + 1e6, 1) = 0;
+       st3(nsp + 1e6, 1) = 0; % if we need to preallocate more space
     end
     
     st3(nsp + [1:nsp0], :) = st';
