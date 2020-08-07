329ef27bc2b115f831315195817fef45e4eb9aa2
Marius Pachitariu <marius10p@gmail.com>
2020-08-07 13:55:04 -0400

    finished datashift comments
    Former-commit-id: e06ddd6545bafd6ada22912d597407a31d68d82e

--- preProcess/datashift2.m
+++ preProcess/datashift2.m
@@ -1,6 +1,6 @@
 function rez = datashift2(rez)
 
-if  getOr(rez.ops, 'datashift', 1)==0
+if  getOr(rez.ops, 'nblocks', 1)==0
     return;
 end
 
@@ -29,85 +29,112 @@ spkTh = 10; % same as the usual "template amplitude", but for the generic templa
 % generic templates. Very few real spikes are missed in this way. 
 st3 = standalone_detector(rez, spkTh);
 
+% binning width across Y (um)
 dd = 5;
+
+% detected depths
 dep = st3(:,2);
+
+% min and max for the range of depths
 dmin = ymin - 1;
 dep = dep - dmin;
 
 dmax  = 1 + ceil(max(dep)/dd);
 Nbatches      = rez.temp.Nbatch;
 
+% which batch each spike is coming from
 batch_id = st3(:,5); %ceil(st3(:,1)/dt);
 
+% preallocate matrix of counts with 20 bins, spaced logarithmically
 F = zeros(dmax, 20, Nbatches);
 for t = 1:Nbatches
+    % find spikes in this batch
     ix = find(batch_id==t);
+    
+    % subtract offset
     dep = st3(ix,2) - dmin;
+    
+    % amplitude bin relative to the minimum possible value
     amp = log10(min(99, st3(ix,3))) - log10(spkTh);
+    
+    % normalization by maximum possible value
     amp = amp / (log10(100) - log10(spkTh));
     
+    % multiply by 20 to distribute a [0,1] variable into 20 bins
+    % sparse is very useful here to do this binning quickly
     M = sparse(ceil(dep/dd), ceil(1e-5 + amp * 20), ones(numel(ix), 1), dmax, 20);    
+    
+    % the counts themselves are taken on a logarithmic scale (some neurons
+    % fire too much!)
     F(:, :, t) = log2(1+M);
 end
 
+%%
+% the 'midpoint' branch is for chronic recordings that have been
+% concatenated in the binary file
 if isfield(ops, 'midpoint')
+    % register the first block as usual
     [imin1, F1] = align_block(F(:, :, 1:ops.midpoint));
+    % register the second block as usual
     [imin2, F2] = align_block(F(:, :, ops.midpoint+1:end));
+    % now register the average first block to the average second block
     d0 = align_pairs(F1, F2);
+    % concatenate the shifts
     imin = [imin1 imin2 + d0];
     imin = imin - mean(imin);
-    ops.datashift = 1;    
+    ops.datashift = 1;
 else
-    switch getOr(ops, 'datashift', 1)
-        case 2
-            ysamp = dmin + dd * [1:dmax] - dd/2;
-            [imin,yblk, F0] = align_block2(F, ysamp);
-        case 1
-            [imin, F0] = align_block(F);
-    end
+    % determine registration offsets 
+    ysamp = dmin + dd * [1:dmax] - dd/2;
+    [imin,yblk, F0] = align_block2(F, ysamp, ops.nblocks);
 end
 
-if getOr(ops, 'fig', 1)
-    
+%%
+if getOr(ops, 'fig', 1)  
     figure(193)
+    % plot the shift trace in um
     plot(imin * dd)
     drawnow
     
     figure;
+    % raster plot of all spikes at their original depths
     st_shift = st3(:,2); %+ imin(batch_id)' * dd;
     for j = spkTh:100
+        % for each amplitude bin, plot all the spikes of that size in the
+        % same shade of gray
         ix = st3(:, 3)==j; % the amplitudes are rounded to integers
-        plot(st3(ix, 1), st_shift(ix), '.', 'color', [1 1 1] * max(0, 1-j/40)) %, 'markersize', j)
+        plot(st3(ix, 1), st_shift(ix), '.', 'color', [1 1 1] * max(0, 1-j/40)) % the marker color here has been carefully tuned
         hold on
     end
     axis tight
 end
 
+% if we're creating a registered binary file for visualization in Phy
 if ~isempty(getOr(ops, 'fbinaryproc', []))
     fid2 = fopen(ops.fbinaryproc, 'w');
     fclose(fid2);
 end
 
+% convert to um 
 dshift = imin * dd;
+% sort in case we still want to do "tracking"
 [~, rez.iorig] = sort(dshift);
 
-%%
+% sigma for the Gaussian process smoothing
 sig = rez.ops.sig;
-
+% register the data batch by batch
 for ibatch = 1:Nbatches
-    switch getOr(ops, 'datashift', 1)
-        case 2
-            shift_batch_on_disk2(rez, ibatch, dshift(ibatch, :), yblk, sig);
-        case 1
-            shift_batch_on_disk(rez, ibatch, dshift(ibatch), sig);
-    end
+    shift_batch_on_disk2(rez, ibatch, dshift(ibatch, :), yblk, sig);
 end
 fprintf('time %2.2f, Shifted up/down %d batches. \n', toc, Nbatches)
 
-
+% keep track of dshift 
 rez.dshift = dshift;
+% keep track of original spikes
 rez.st0 = st3;
 
+
+% next, we can just run a normal spike sorter, like Kilosort1, and forget about the transformation that has happened in here 
 
 %%
 
--- preProcess/shift_batch_on_disk2.m
+++ preProcess/shift_batch_on_disk2.m
@@ -1,69 +1,44 @@
-function [dat_cpu, dat, shifts] = shift_batch_on_disk2(rez, ibatch, shift, ysamp, sig)
-% this function finds threshold crossings in the data using
-% projections onto the pre-determined principal components
-% wPCA is number of time samples by number of PCs
-% ibatch is a scalar indicating which batch to analyze
-% iC is NchanNear by Nchan, indicating for each channel the nearest
-% channels to it
-
-
-% ibatch = 1;
-% shift = dshift(ibatch);
+function [dat_cpu, dat, shifts] = shift_batch_on_disk2(rez, ibatch, shifts, ysamp, sig)
+% register one batch of a whitened binary file
 
 ops = rez.ops;
 Nbatch      = rez.temp.Nbatch;
 NT  	      = ops.NT;
 
-
 batchstart = 0:NT:NT*Nbatch; % batches start at these timepoints
 offset = 2 * ops.Nchan*batchstart(ibatch); % binary file offset in bytes
 
-
-shifts = interp1(ysamp, shift, rez.yc, 'makima', 'extrap');
-
+% upsample the shift for each channel using interpolation
+if length(ysamp)>1
+    shifts = interp1(ysamp, shifts, rez.yc, 'makima', 'extrap');
+end
+% load the batch
 fclose all;
 fid = fopen(ops.fproc, 'r+');
 fseek(fid, offset, 'bof');
 dat = fread(fid, [NT ops.Nchan], '*int16');
 
+% 2D coordinates for interpolation 
 xp = cat(2, rez.xc, rez.yc);
-% sig = 15;
 
+% 2D kernel of the original channel positions 
 Kxx = kernel2D(xp, xp, sig);
+% 2D kernel of the new channel positions
 yp = xp;
 yp(:, 2) = yp(:, 2) - shifts; % * sig;
-
 Kyx = kernel2D(yp, xp, sig);
 
+% kernel prediction matrix
 M = Kyx /(Kxx + .01 * eye(size(Kxx,1)));
-% M = (rez.Wrot' * M) / rez.Wrot';
 
+% the multiplication has to be done on the GPU
 dati = gpuArray(single(dat)) * gpuArray(M)';
-
-% ishift = round(shift);
-% shift = shift - ishift;
-
-% dat = reshape(dat, [NT, 2, ops.Nchan/2]);
-% dat = circshift(dat, ishift, 3);
-% 
-% if shift > 0
-%     alpha = shift;
-%     datup = circshift(dat, 1, 3);
-%     dat = dat * (1-alpha) + alpha * datup;
-% else
-%     alpha = -shift;
-%     datup = circshift(dat, -1, 3);
-%     dat = dat * (1-alpha) + alpha * datup;
-% end
-% 
-% 
-% if alpha<0 || alpha>1
-%    disp(alpha)
-% end
 
 dat_cpu = gather(int16(dati));
 
 if ~isempty(getOr(ops, 'fbinaryproc', []))
+    % if the user wants to have a registered version of the binary file 
+    % this one is not split into batches
     fid2 = fopen(ops.fbinaryproc, 'a');
     ifirst = ops.ntbuff+1;
     ilast = ops.NT;
@@ -76,6 +51,7 @@ if ~isempty(getOr(ops, 'fbinaryproc', []))
 end
 
 if nargout==0
+    % normally we want to write the aligned data back to the same file
     fseek(fid, offset, 'bof');
     fwrite(fid, dat_cpu, 'int16'); % write this batch to binary file
 end
