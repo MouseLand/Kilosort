82b6aca921ff7ffdc7b9734da81471ab1903f76b
Adrian Bondy <adrian.bondy@gmail.com>
2020-08-28 17:28:04 -0400

    bug fixes
    Former-commit-id: 79e7dcb66be21ffa372ee21298760f0116302998

--- postProcess/remove_ks2_duplicate_spikes.m
+++ postProcess/remove_ks2_duplicate_spikes.m
@@ -4,9 +4,9 @@
 % 
 % This function takes the kilosort2 output rez and identifies pair of
 % spikes that are close together in time and space. The temporal threshold
-% is give by the parameter OVERLAP_S which is 5e-4 (0.5ms by default) and
+% is give by the parameter OVERLAP_S which is 0.5ms by default and
 % the spatial threshold (applied to the template primary sites) is given by
-% CHANNEL_SEPARATION_UM and is 50 by default.
+% CHANNEL_SEPARATION_UM and is 75 by default.
 %
 % From these spike pairs, it identifies the pair with the larger template as
 % being the "main" or "reference" cluster and the duplicate spikes from the
@@ -36,8 +36,8 @@
 %   >> rez = remove_ks2_duplicate_spikes(rez)
 function rez = remove_ks2_duplicate_spikes(rez, varargin)
     input_parser = inputParser;
-    addParameter(input_parser, 'overlap_s', 5e-4, @(x) (isnumeric(x)))
-    addParameter(input_parser, 'channel_separation_um', 50, @(x) (ischar(x)))
+    addParameter(input_parser, 'overlap_s', 5e-4, @(x) (isnumeric(x))) % the temporal window within which pairs of spikes will be considered duplicates (if they are also within the spatial window)
+    addParameter(input_parser, 'channel_separation_um', 75, @(x) (ischar(x))) % the spatial window within which pairs of spikes will be considered duplicates (if they are also within the temporal window)
     parse(input_parser, varargin{:});
     P = input_parser.Results;
 
@@ -51,11 +51,13 @@ function rez = remove_ks2_duplicate_spikes(rez, varargin)
        templates(:,:,iNN) = squeeze(rez.U(:,iNN,:)) * squeeze(rez.W(:,iNN,:))';
     end
     templates = permute(templates, [3 2 1]); % now it's nTemplates x nSamples x nChannels
-
+    n_spikes=numel(spike_times);        
     %% Make sure that the spike times are sorted
     if ~issorted(spike_times)
-        [spike_times, I] = sort(spike_times);
-        spike_templates = spike_templates(I);
+        [spike_times, spike_idx] = sort(spike_times);
+        spike_templates = spike_templates(spike_idx);
+    else
+        spike_idx=(1:n_spikes)';
     end
     %% deal with cluster 0
     if any(spike_templates==0)
@@ -89,59 +91,44 @@ function rez = remove_ks2_duplicate_spikes(rez, varargin)
     n_samples_overlap = cast(n_samples_overlap, class(spike_times));
     %% Distance between each channel
     chan_dist = ((rez.xcoords - rez.xcoords').^2 + (rez.ycoords - rez.ycoords').^2).^0.5;
-    %imagesc(chan_dist)
-
     n_spikes=numel(spike_times);
-    n_duplicates=1;  % set to 1 to initialize while loop
-    count=0;
     remove_idx = [];
     reference_idx = [];
-    spike_idx = [1:n_spikes]';
-    current_spike_times = spike_times;
-    current_spike_idx = spike_idx;
-    current_primaries = spike_primary;
-    % only check nearest temporal neighbors in the list of spikes times.
-    % but go recursively until no nearest neighbors are left that are both within the overlap
-    % period and sufficiently nearby. 
-    % this means only ever computing a vector operation (i.e. diff(spike_times))
+    % check pairs of spikes in the time-ordered list for being close together in space and time. 
+    % Check pairs that are separated by N other spikes,
+    % starting with N=0. Increasing N until there are no spikes within the temporal overlap window. 
+    % This means only ever computing a vector operation (i.e. diff(spike_times))
     % rather than a matrix one (i.e. spike_times - spike_times').
-    diff_order=1;
+    diff_order=0;
     while 1==1
-        count=count+1;
-        if n_duplicates==0
-            diff_order=diff_order+1;
-            fprintf('No duplicates but simultaneous spikes haven''t been fully explored.\nNow comparing spikes separated by %g other spikes.\n',diff_order-1);
-        end
-        keep_idx = ~ismember(spike_idx,remove_idx);
-        current_spike_idx = spike_idx(keep_idx);
-        current_spike_times = spike_times(keep_idx); 
-        current_primaries = spike_primary(keep_idx);
-        isis=current_spike_times(1+diff_order:end) - current_spike_times(1:end-diff_order);
+        diff_order=diff_order+1;
+        fprintf('Now comparing spikes separated by %g other spikes.\n',diff_order-1);
+        isis=spike_times(1+diff_order:end) - spike_times(1:end-diff_order);
         simultaneous = isis<n_samples_overlap;
         if any(isis<0)
-            error('ISIs less than zero? Something is wrong.');
+            error('ISIs less than zero? Something is wrong because spike times should be sorted.');
         end
         if ~any(simultaneous)
             fprintf('No remaining simultaneous spikes.\n');
             break
         end
-        nearby = chan_dist(sub2ind(size(chan_dist),current_primaries(1:end-diff_order),current_primaries(1+diff_order:end)))<P.channel_separation_um;
-        first_duplicate = find(simultaneous & nearby); % indexes the first member of the pair
+        nearby = chan_dist(sub2ind(size(chan_dist),spike_primary(1:end-diff_order),spike_primary(1+diff_order:end)))<P.channel_separation_um;
+        first_duplicate = find(simultaneous & nearby); % indexes the first (earliest in time) member of the pair
         n_duplicates = length(first_duplicate);
         if ~isempty(first_duplicate)
-            fprintf('On iteration %g, %g duplicate spike pairs were identified.\n',count,n_duplicates);
-            amps_to_compare=tempAmpsUnscaled(current_primaries([first_duplicate first_duplicate(:)+diff_order]));
+            fprintf('On iteration %g, %g duplicate spike pairs were identified.\n',diff_order,n_duplicates);
+            amps_to_compare=tempAmpsUnscaled(spike_primary([first_duplicate first_duplicate(:)+diff_order]));
             if length(first_duplicate)==1
                 amps_to_compare = amps_to_compare(:)'; % special case requiring a dimension change
             end
             first_is_bigger =  diff(amps_to_compare,[],2)<=0;
-            remove_idx = [remove_idx ; current_spike_idx([first_duplicate(~first_is_bigger);(first_duplicate(first_is_bigger)+diff_order)])];
-            reference_idx = [reference_idx ; current_spike_idx([(first_duplicate(~first_is_bigger)+diff_order);first_duplicate(first_is_bigger)])];
-            [remove_idx,idx] = unique(remove_idx);
-            reference_idx = reference_idx(idx);
+            remove_idx = [remove_idx ; spike_idx([first_duplicate(~first_is_bigger);(first_duplicate(first_is_bigger)+diff_order)])];
+            reference_idx = [reference_idx ; spike_idx([(first_duplicate(~first_is_bigger)+diff_order);first_duplicate(first_is_bigger)])];
         end
     end
-    logical_remove_idx = ismember(spike_idx,remove_idx);
+    [remove_idx,idx] = unique(remove_idx);
+    reference_idx = reference_idx(idx);    
+    logical_remove_idx = ismember((1:n_spikes)',remove_idx);
     rez = remove_spikes(rez,logical_remove_idx,'duplicate','reference_time',spike_times(reference_idx),...
         'reference_cluster',spike_templates(reference_idx));
 end
