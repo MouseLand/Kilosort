84682e1ead8229ee311e8a445be2b3b7fd802bb0
Marius <marius10p@gmail.com>
2021-02-05 10:45:26 -0500

    groups for 2D geometries and arbitrary chan sortings
    Former-commit-id: d8942c664ebded8c14dca67b4a160d34c5cbb34c

--- clustering/template_learning.m
+++ clustering/template_learning.m
@@ -29,7 +29,12 @@ tmp_chan = iC(1, :);
 ss = double(st3(:,1)) / ops.fs;
 
 dmin = rez.ops.dmin;
-ycenter = (min(rez.yc) + dmin-5):(2*dmin):(max(rez.yc)+dmin+5);
+ycenter = (min(rez.yc) + dmin-1):(2*dmin):(max(rez.yc)+dmin+1);
+dminx = rez.ops.dminx;
+xcenter = (min(rez.xc) + dminx-1):(2*dminx):(max(rez.xc)+dminx+1);
+[xcenter, ycenter] = meshgrid(xcenter, ycenter);
+xcenter = xcenter(:);
+ycenter = ycenter(:);
 
 Wpca = zeros(6, Nchan, 1000, 'single');
 nst = numel(ktid);
@@ -39,16 +44,15 @@ hid = zeros(nst,1 , 'int32');
 
 
 tic
+
 for j = 1:numel(ycenter)
     if rem(j,5)==1
         fprintf('time %2.2f, GROUP %d/%d, units %d \n', toc, j, numel(ycenter), n0)    
     end
-    y0 = ycenter(j);
-%     y0 = 700;
-%     xchan = abs(rez.yc - y0) < 20;
-%     itemp = find(xchan(tmp_chan));
     
-    xchan = abs(ycup - y0) < dmin;
+    y0 = ycenter(j);
+    x0 = xcenter(j);    
+    xchan = (abs(ycup - y0) < dmin) & (abs(xcup - x0) < dminx);
     itemp = find(xchan);
         
     if isempty(itemp)
@@ -65,25 +69,29 @@ for j = 1:numel(ycenter)
     
     
     ich = unique(iC(:, itemp));
-    ch_min = ich(1)-1;
-    ch_max = ich(end);
+%     ch_min = ich(1)-1;
+%     ch_max = ich(end);
     
-    if ch_max-ch_min<3
+    if numel(ich)<1
         continue;
     end
     
     nsp = size(data,1);
-    dd = zeros(nsp, 6, ch_max-ch_min, 'single');
+    dd = zeros(nsp, 6, numel(ich), 'single');
     for k = 1:length(itemp)
         ix = pid==itemp(k);
-        dd(ix, :, iC(:,itemp(k))-ch_min) = data(ix,:,:);
+        % how to go from channels to different order
+        [~,ia,ib] = intersect(iC(:,itemp(k)), ich);
+        dd(ix, :, ib) = data(ix,:,ia);
     end
 
     kid = run_pursuit(dd, nlow, rmin, n0, wroll, ss(tin), use_CCG);
-    
+
+    [~, ~, kid] = unique(kid);
     nmax = max(kid);
     for t = 1:nmax
-        Wpca(:, ch_min+1:ch_max, t + n0) = gather(sq(mean(dd(kid==t,:,:),1)));
+%         Wpca(:, ch_min+1:ch_max, t + n0) = gather(sq(mean(dd(kid==t,:,:),1)));
+        Wpca(:, ich, t + n0) = gather(sq(mean(dd(kid==t,:,:),1)));
     end
     
     hid(tin) = gather(kid + n0);
