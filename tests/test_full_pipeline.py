b7c9c045ac36494719a561ebc9552e63b2ed2d23
jacobpennington <jacob.pennington@wsu.edu>
2023-03-30 14:00:41 -0700

    fixed pipeline test, now works with sample data

--- tests/test_full_pipeline.py
+++ tests/test_full_pipeline.py
@@ -10,26 +10,25 @@ from kilosort import run_kilosort, default_settings
 @pytest.mark.slow
 def test_pipeline():
     settings = default_settings()
-    # TODO: replace this with a dedicated test dataset that's stored inside
-    #       test directory so that path can be determined dynamically
+    # TODO: replace this with a pytest fixture that downloads test data from
+    #       remote repository. 
     settings['data_dir'] = "C:/code/kilosort4_data/"
     # TODO: add option to not save results to file? Otherwise this will need
-    #       to overwrite some results files every time, which is unnecessary.
-    # ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate = \
-    #   run_kilosort(settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat')
+    #       to store and overwrite some results files every time, which is
+    #       unnecessary.
     _, st, clu, _, _, _, _, _ = run_kilosort(
-        settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat',
-        save=False
+        settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat'
         )
+    st = st[:,0]  # only first column is spike times, that's all that gets saved
         
     # Check that spike times and spike cluster assignments match
-    results_dir = Path(settings['data_dir']).joinpath('kilosort4')
+    results_dir = Path(settings['data_dir']).joinpath('pytest/')
     st_load = np.load(results_dir / 'spike_times.npy')
     clu_load = np.load(results_dir / 'spike_clusters.npy')
 
     assert np.allclose(st, st_load)
     assert np.allclose(clu, clu_load)
-    # TODO: What else? Or is that sufficient?
+    # TODO: What else? Or is that sufficient for now?
     
 
 def test_step_by_step():
