5047a49cd49861970c22959245703033341bf5a1
jacobpennington <jacob.pennington@wsu.edu>
2023-04-07 13:46:06 -0700

    Added data fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
+from pathlib import Path
+
+import numpy as np
 import pytest
+import torch
+
+import kilosort.preprocessing as kpp
+from kilosort import io
+from kilosort import PROBE_DIR
 
 
+# TODO: rename this so that flag is something more informative like
+#       "--regression"
 # runslow flag configured according to response from Manu CJ here:
 # https://stackoverflow.com/questions/47559524/pytest-how-to-skip-tests-unless-you-declare-an-option-flag
 def pytest_addoption(parser):
@@ -20,4 +30,43 @@ def pytest_collection_modifyitems(config, items):
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
 
-# TODO: pytest fixtures here.
+
+@pytest.fixture()
+def torch_device():
+    return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+
+# TODO: have a short sample version and a full version
+# TODO: also download probe file if not already present
+@pytest.fixture()
+def data_directory():
+    """Specifies directory for test data and pre-computed results."""
+    data_path = Path("C:/code/kilosort4_data/")
+    # TODO: move this to a remote repository, hard-coded for Jacob's PC for now.
+
+    return data_path
+
+@pytest.fixture()
+def results_directory(data_directory):
+    return data_directory.joinpath("pytest/")
+
+@pytest.fixture()
+def saved_ops(results_directory):
+    ops = np.load(results_directory / 'ops.npy', allow_pickle=True).item()
+    return ops
+
+@pytest.fixture()
+def bfile(saved_ops, torch_device):
+    # TODO: add option to load BinaryFiltered from ops dict, move this code
+    #       to that function
+    settings = saved_ops['settings']
+
+    bfile = io.BinaryFiltered(
+        settings['filename'], settings['n_chan_bin'], settings['fs'],
+        settings['NT'], settings['nt'], settings['nt0min'],
+        saved_ops['probe']['chanMap'], hp_filter=saved_ops['fwav'],
+        whiten_mat=saved_ops['Wrot'], dshift=saved_ops['dshift'],
+        device=torch_device
+        )
+    
+    return bfile
--- tests/test_full_pipeline.py
+++ tests/test_full_pipeline.py
@@ -2,37 +2,40 @@ from pathlib import Path
 
 import numpy as np
 import pytest
+import torch
 
 from kilosort import run_kilosort, default_settings
 
 
 # Use `pytest --runslow` option to include this in tests.
 @pytest.mark.slow
-def test_pipeline():
+def test_pipeline(data_directory, results_directory, saved_ops, torch_device):
     settings = default_settings()
     # TODO: replace this with a pytest fixture that downloads test data from
     #       remote repository. 
-    settings['data_dir'] = "C:/code/kilosort4_data/"
+    settings['data_dir'] = data_directory
     # TODO: add option to not save results to file? Otherwise this will need
     #       to store and overwrite some results files every time, which is
     #       unnecessary.
-    _, st, clu, _, _, _, _, _ = run_kilosort(
-        settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat'
+    ops, st, clu, _, _, _, _, _ = run_kilosort(
+        settings=settings, probe_name='neuropixPhase3B1_kilosortChanMap.mat',
+        device=torch_device
         )
     st = st[:,0]  # only first column is spike times, that's all that gets saved
         
     # Check that spike times and spike cluster assignments match
-    results_dir = Path(settings['data_dir']).joinpath('pytest/')
-    st_load = np.load(results_dir / 'spike_times.npy')
-    clu_load = np.load(results_dir / 'spike_clusters.npy')
+    st_load = np.load(results_directory / 'spike_times.npy')
+    clu_load = np.load(results_directory / 'spike_clusters.npy')
+    saved_yblk = saved_ops['yblk']
+    saved_dshift = saved_ops['dshift']
+    saved_iKxx = saved_ops['iKxx'].to(torch_device)
 
+    # Datashift output
+    assert np.allclose(saved_yblk, ops['yblk'])
+    assert np.allclose(saved_dshift, ops['dshift'])
+    assert torch.allclose(saved_iKxx, ops['iKxx'])
+    # Final spike/neuron readout
     assert np.allclose(st, st_load)
     assert np.allclose(clu, clu_load)
     # TODO: What else? Or is that sufficient for now?
     
-
-def test_step_by_step():
-    # TODO: As above, but break out into individual steps with result of each
-    #       operation pre-saved, so that if there's a mismatch it's easy to
-    #       identify where it's coming from.
-    pass
