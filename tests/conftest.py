9583cf13584cdc4c4bbbf2b976511c2f2f5e3c0b
jacobpennington <jacob.pennington@wsu.edu>
2023-04-21 16:54:35 -0700

    Added tests for new dtype options

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,7 +35,7 @@ def pytest_collection_modifyitems(config, items):
 ### Configure data paths, download data if not already present.
 # Adapted from https://github.com/MouseLand/suite2p/blob/main/conftest.py
 # TODO: also download probe file if not already present
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def data_directory():
     """Specifies directory for test data and results, downloads if needed."""
 
@@ -70,8 +70,9 @@ def download_data(local, remote):
     with zipfile.ZipFile(zip_file, "r") as zip_ref:
         zip_ref.extractall(local.parent)
 
-
-# @retry  # TODO: look at tenacity package, determine if this is necessary
+# TODO: look at tenacity package, determine if this is necessary, would introduce
+#       another dependency
+# @retry
 def download_url_to_file(url, dst, progress=True):
     """Download object at the given URL to a local path.
     
@@ -128,11 +129,12 @@ def download_url_to_file(url, dst, progress=True):
     finally:
         # Close and delete temporary file
         f.close()
+        p = Path(f.name)
         if f.name.is_file():
             Path(f.name).unlink()
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def results_directory(data_directory):
     return data_directory.joinpath("pytest/")
 
@@ -140,16 +142,16 @@ def results_directory(data_directory):
 
 
 ### Load data for use by unit and regression tests
-@pytest.fixture()
+@pytest.fixture(scope='session')
+def torch_device():
+    return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+@pytest.fixture(scope='session')
 def saved_ops(results_directory):
     ops = np.load(results_directory / 'ops.npy', allow_pickle=True).item()
     return ops
 
-@pytest.fixture()
-def torch_device():
-    return torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def bfile(saved_ops, torch_device):
     # TODO: add option to load BinaryFiltered from ops dict, move this code
     #       to that function
