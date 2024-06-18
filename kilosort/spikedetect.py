Fixed spike time overflow in spikedetect.run

kilosort/spikedetect.py
@@ -261,7 +261,7 @@ def run(ops, bfile, device=torch.device('cuda'), progress_bar=None):
         xfeat = xsub @ ops['wPCA'].T
         tF[k:k+nsp] = xfeat.transpose(0,1).cpu().numpy()
 
-        st[k:k+nsp,0] = ((xy[:,1]-nt)/ops['fs'] + ibatch * (ops['batch_size']/ops['fs'])).cpu().numpy()
+        st[k:k+nsp,0] = ((xy[:,1].cpu().numpy()-nt)/ops['fs'] + ibatch * (ops['batch_size']/ops['fs']))
         st[k:k+nsp,1] = yct.cpu().numpy()
         st[k:k+nsp,2] = amp.cpu().numpy()
         st[k:k+nsp,3] = imax.cpu().numpy()