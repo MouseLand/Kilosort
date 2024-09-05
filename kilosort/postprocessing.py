a2d745f1b3842f1b37b43e42284d658d49a67ea7
jacobpennington <jacob.pennington@wsu.edu>
2024-09-05 12:23:15 -0700

    Fixed bug in make_pc_features for templates w no spikes

--- kilosort/postprocessing.py
+++ kilosort/postprocessing.py
@@ -81,6 +81,7 @@ def make_pc_features(ops, spike_templates, spike_clusters, tF):
 
     # xy: template centers, iC: channels associated with each template
     xy, iC = xy_templates(ops)
+    n_templates = iC.shape[1]
     n_clusters = np.unique(spike_clusters).size
     n_chans = ops['nearest_chans']
     feature_ind = np.zeros((n_clusters, n_chans), dtype=np.uint32)
@@ -89,7 +90,7 @@ def make_pc_features(ops, spike_templates, spike_clusters, tF):
         # Get templates associated with cluster (often just 1)
         iunq = np.unique(spike_templates[spike_clusters==i]).astype(int)
         # Get boolean mask with size (n_templates,), True if they match cluster
-        ix = torch.from_numpy(np.zeros(int(spike_templates.max())+1, bool))
+        ix = torch.from_numpy(np.zeros(n_templates, bool))
         ix[iunq] = True
         # Get PC features for all spikes detected with those templates (Xd),
         # and the indices in tF where those spikes occur (igood).
