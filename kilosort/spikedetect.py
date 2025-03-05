fa03081212c75c47035a00e43db97420a7117c1c
Roberto <37729096+RobertoDF@users.noreply.github.com>
2025-03-05 12:59:36 +0100

    Update spikedetect.py

--- kilosort/spikedetect.py
+++ kilosort/spikedetect.py
@@ -75,12 +75,10 @@ def extract_wPCA_wTEMP(ops, bfile, nt=61, twav_min=20, Th_single_ch=6, nskip=25,
         warnings.filterwarnings("ignore", message="")
         # Prevents memory leak for KMeans when using MKL on Windows
         msg = 'KMeans is known to have a memory leak on Windows with MKL'
-        nthread = os.environ.get('OMP_NUM_THREADS', msg)
-        os.environ['OMP_NUM_THREADS'] = '7'
+        nthread = os.environ.get('OMP_NUM_THREADS', "7")
         model = KMeans(n_clusters=ops['settings']['n_templates'], n_init = 10).fit(clips)
         wTEMP = torch.from_numpy(model.cluster_centers_).to(device).float()
         wTEMP = wTEMP / (wTEMP**2).sum(1).unsqueeze(1)**.5
-        os.environ['OMP_NUM_THREADS'] = nthread
 
     return wPCA, wTEMP
 
