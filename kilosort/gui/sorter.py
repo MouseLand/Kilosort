944f833ce5c5a6aa8d4c395e70ef16c756ad2c63
Jacob Pennington <penningtonj2@hhmi.org>
2025-04-23 16:39:11 -0400

    Removed no-longer-needed code from gui sorter

--- kilosort/gui/sorter.py
+++ kilosort/gui/sorter.py
@@ -1,21 +1,10 @@
-import time
 import logging
 logger = logging.getLogger(__name__)
 
-import numpy as np
 import torch
 from qtpy import QtCore
 
-import kilosort
-from kilosort.run_kilosort import (
-    # setup_logger, initialize_ops, compute_preprocessing, compute_drift_correction,
-    # detect_spikes, cluster_spikes, save_sorting, close_logger
-    setup_logger, _sort
-    )
-from kilosort.io import save_preprocessing
-from kilosort.utils import (
-    log_performance, log_cuda_details, ops_as_string, probe_as_string
-    )
+from kilosort.run_kilosort import setup_logger, _sort
 
 
 class KiloSortWorker(QtCore.QThread):
@@ -43,9 +32,7 @@ class KiloSortWorker(QtCore.QThread):
             settings["data_dir"] = self.data_path[0].parent
             settings["filename"] = self.data_path
             results_dir = self.results_directory
-            if not results_dir.exists():
-                results_dir.mkdir(parents=True)
-            
+            results_dir.mkdir(parents=True, exist_ok=True)
             setup_logger(results_dir)
 
             # NOTE: All but `gui_sorter` are positional args,
@@ -57,7 +44,7 @@ class KiloSortWorker(QtCore.QThread):
                 settings['save_preprocessed_copy'], settings['verbose_log'],
                 False, self.file_object, self.progress_bar, gui_sorter=self
                 )
-            # Hard-coded `False` is for "save_extra_vars", which isn't an option
-            # in the GUI right now (and isn't likely to be added).
+            # Hard-coded `False` is for "save_extra_vars", which isn't an
+            # option in the GUI right now (and isn't likely to be added).
 
             self.finishedSpikesort.emit(self.context)
